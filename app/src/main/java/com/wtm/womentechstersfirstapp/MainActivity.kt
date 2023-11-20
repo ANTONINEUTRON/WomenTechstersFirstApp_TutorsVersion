6fdcf2c22f9271efa713760bfa4623b3192505ee
AntoniNeutron <antonineutron@gmail.com>
2023-11-20 13:41:27 +0100

    Features for 20th November Implemented

--- app/src/main/java/com/wtm/womentechstersfirstapp/MainActivity.kt
+++ app/src/main/java/com/wtm/womentechstersfirstapp/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.wtm.womentechstersfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        var jnBtn: Button = findViewById(R.id.joinNowButton)
+        //Access the button on our UI
+        val joinButton: Button = findViewById(R.id.joinNowButton)
+
+        //Set a listener to the button
+        joinButton.setOnClickListener{
+            //Navigate user to LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
