5c2a509516f53736317556cdd2e1c053eed71dda
AntoniNeutron <antonineutron@gmail.com>
2023-11-20 12:53:01 +0100

    Initial Commit

---
+++ app/src/main/java/com/wtm/womentechstersfirstapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.wtm.womentechstersfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        var jnBtn: Button = findViewById(R.id.joinNowButton)
+    }
+}
