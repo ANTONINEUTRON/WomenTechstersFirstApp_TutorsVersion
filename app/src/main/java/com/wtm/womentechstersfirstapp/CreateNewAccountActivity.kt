a5faf827dbb549787e3cdfb2d6f8cc14bdf65332
AntoniNeutron <antonineutron@gmail.com>
2023-11-21 14:04:38 +0100

    November 21st class Done

---
+++ app/src/main/java/com/wtm/womentechstersfirstapp/CreateNewAccountActivity.kt
@@ -0,0 +1,11 @@
+package com.wtm.womentechstersfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateNewAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_new_account)
+    }
+}
--- app/src/main/java/com/wtm/womentechstersfirstapp/LoginActivity.kt
+++ app/src/main/java/com/wtm/womentechstersfirstapp/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.wtm.womentechstersfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var createNAButton: Button = findViewById(R.id.createNewAccount)
+
+        createNAButton.setOnClickListener {
+            var i = Intent(this, CreateNewAccountActivity::class.java)
+
+            startActivity(i)
+        }
     }
 }
