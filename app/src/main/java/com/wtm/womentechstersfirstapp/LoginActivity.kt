f9987048c11f7672fd72e670edea8f36381d3d87
AntoniNeutron <antonineutron@gmail.com>
2023-11-22 11:59:56 +0100

    November 22nd features implemented by lunch break

--- app/src/main/java/com/wtm/womentechstersfirstapp/LoginActivity.kt
+++ app/src/main/java/com/wtm/womentechstersfirstapp/LoginActivity.kt
@@ -11,11 +11,25 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         var createNAButton: Button = findViewById(R.id.createNewAccount)
-
         createNAButton.setOnClickListener {
             var i = Intent(this, CreateNewAccountActivity::class.java)
-
             startActivity(i)
         }
+
+        val forgotPasswordBtn: Button = findViewById(R.id.forgottenPassword)
+        forgotPasswordBtn.setOnClickListener {
+            var i = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(i)
+        }
+
+        val loginBtn: Button = findViewById(R.id.loginButton)
+        loginBtn.setOnClickListener{
+            navigateToLoginPage()
+        }
+    }
+
+    private fun navigateToLoginPage() {
+        var intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
