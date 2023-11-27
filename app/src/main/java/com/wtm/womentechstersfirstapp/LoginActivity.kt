f0bc4e30374b89ccc46f89d63c8eacbdb74e3af8
AntoniNeutron <antonineutron@gmail.com>
2023-11-27 13:37:41 +0100

    November 27th

--- app/src/main/java/com/wtm/womentechstersfirstapp/LoginActivity.kt
+++ app/src/main/java/com/wtm/womentechstersfirstapp/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +25,31 @@ class LoginActivity : AppCompatActivity() {
         }
 
         val loginBtn: Button = findViewById(R.id.loginButton)
+        val username: EditText = findViewById(R.id.username)
+        val password: EditText = findViewById(R.id.password)
+
         loginBtn.setOnClickListener{
-            navigateToLoginPage()
+            var usernameTxt: String = username.text.toString()
+            var passwordTxt: String = password.text.toString()
+            var message: String = ""
+
+            //perform validation checks
+            if(usernameTxt != "Fauziyat"){
+                message = "Wrong Username"
+            }else if(passwordTxt != "demo"){
+                message = "Invalid Password"
+            }
+
+            //show message or take user to homepage
+            if(message.isNotEmpty()){
+                Snackbar.make(
+                    findViewById(R.id.layout),
+                    message,
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }else {
+                navigateToLoginPage()
+            }
         }
     }
 
